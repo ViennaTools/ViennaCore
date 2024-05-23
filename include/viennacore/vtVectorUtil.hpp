4e7dfab30cfeb6796a35cf6d42e2789003e34237
Tobias Reiter <t.reiter1@live.de>
2024-05-22 20:54:51 -0400

    Refactor vector util

--- include/viennacore/vtVectorUtil.hpp
+++ include/viennacore/vtVectorUtil.hpp
@@ -14,36 +14,48 @@ template <typename NumericType> using Triple = std::array<NumericType, 3>;
 template <typename NumericType> using Quadruple = std::array<NumericType, 4>;
 
 /* ------------- Vector operation functions ------------- */
-template <typename NumericType>
-[[nodiscard]] Triple<NumericType> Sum(const Triple<NumericType> &pVecA,
-                                      const Triple<NumericType> &pVecB) {
-  return {pVecA[0] + pVecB[0], pVecA[1] + pVecB[1], pVecA[2] + pVecB[2]};
+template <typename NumericType, std::size_t D>
+[[nodiscard]] std::array<NumericType, D>
+Sum(const std::array<NumericType, D> &pVecA,
+    const std::array<NumericType, D> &pVecB) {
+  std::array<NumericType, D> rr;
+  for (size_t i = 0; i < D; ++i) {
+    rr[i] = pVecA[i] + pVecB[i];
+  }
+  return rr;
 }
 
-template <typename NumericType>
-[[nodiscard]] Triple<NumericType> Sum(const Triple<NumericType> &pVecA,
-                                      const Triple<NumericType> &pVecB,
-                                      const Triple<NumericType> &pT) {
-  return {pVecA[0] + pVecB[0] + pT[0], pVecA[1] + pVecB[1] + pT[1],
-          pVecA[2] + pVecB[2] + pT[2]};
+template <typename NumericType, std::size_t D>
+[[nodiscard]] std::array<NumericType, D>
+Sum(const std::array<NumericType, D> &pVecA,
+    const std::array<NumericType, D> &pVecB,
+    const std::array<NumericType, D> &pVecC) {
+  std::array<NumericType, D> rr;
+  for (size_t i = 0; i < D; ++i) {
+    rr[i] = pVecA[i] + pVecB[i] + pVecC[i];
+  }
+  return rr;
 }
 
-template <typename NumericType>
-[[nodiscard]] Triple<NumericType> Diff(const Triple<NumericType> &pVecA,
-                                       const Triple<NumericType> &pVecB) {
-  return {pVecA[0] - pVecB[0], pVecA[1] - pVecB[1], pVecA[2] - pVecB[2]};
+template <typename NumericType, std::size_t D>
+[[nodiscard]] std::array<NumericType, D>
+Diff(const std::array<NumericType, D> &pVecA,
+     const std::array<NumericType, D> &pVecB) {
+  std::array<NumericType, D> rr;
+  for (size_t i = 0; i < D; ++i) {
+    rr[i] = pVecA[i] - pVecB[i];
+  }
+  return rr;
 }
 
-template <typename NumericType>
-[[nodiscard]] Pair<NumericType> Diff(const Pair<NumericType> &pVecA,
-                                     const Pair<NumericType> &pVecB) {
-  return {pVecA[0] - pVecB[0], pVecA[1] - pVecB[1]};
-}
-
-template <typename NumericType>
-[[nodiscard]] NumericType DotProduct(const Triple<NumericType> &pVecA,
-                                     const Triple<NumericType> &pVecB) {
-  return pVecA[0] * pVecB[0] + pVecA[1] * pVecB[1] + pVecA[2] * pVecB[2];
+template <typename NumericType, std::size_t D>
+[[nodiscard]] NumericType DotProduct(const std::array<NumericType, D> &pVecA,
+                                     const std::array<NumericType, D> &pVecB) {
+  NumericType dot = 0;
+  for (size_t i = 0; i < D; ++i) {
+    dot += pVecA[i] * pVecB[i];
+  }
+  return dot;
 }
 
 template <typename NumericType>
@@ -57,7 +69,7 @@ CrossProduct(const Triple<NumericType> &pVecA,
   return rr;
 }
 
-template <typename NumericType, size_t D>
+template <typename NumericType, std::size_t D>
 [[nodiscard]] NumericType Norm(const std::array<NumericType, D> &vec) {
   NumericType norm = 0;
   std::for_each(vec.begin(), vec.end(),
@@ -65,7 +77,7 @@ template <typename NumericType, size_t D>
   return std::sqrt(norm);
 }
 
-template <typename NumericType, size_t D>
+template <typename NumericType, std::size_t D>
 void Normalize(std::array<NumericType, D> &vec) {
   NumericType norm = 1. / Norm(vec);
   if (norm == 1.)
@@ -87,37 +99,49 @@ Normalize(const std::array<NumericType, D> &vec) {
   return normedVec;
 }
 
-template <typename T, size_t D>
-void ScaleToLength(std::array<T, D> &vec, const T length) {
+template <typename NumericType, std::size_t D>
+void ScaleToLength(std::array<NumericType, D> &vec, const NumericType length) {
   const auto vecLength = Norm(vec);
   for (size_t i = 0; i < D; i++)
     vec[i] *= length / vecLength;
 }
 
-template <typename NumericType>
-[[nodiscard]] Triple<NumericType> Inv(const Triple<NumericType> &vec) {
-  return {-vec[0], -vec[1], -vec[2]};
+template <typename NumericType, std::size_t D>
+[[nodiscard]] std::array<NumericType, D>
+Inv(const std::array<NumericType, D> &vec) {
+  std::array<NumericType, D> rr;
+  for (size_t i = 0; i < D; ++i) {
+    rr[i] = -vec[i];
+  }
+  return rr;
 }
 
-template <typename NumericType>
-void Scale(const NumericType pF, Triple<NumericType> &pT) {
-  pT[0] *= pF;
-  pT[1] *= pF;
-  pT[2] *= pF;
+template <typename NumericType, std::size_t D>
+void Scale(const NumericType pF, std::array<NumericType, D> &pT) {
+  for (size_t i = 0; i < D; ++i) {
+    pT[i] *= pF;
+  }
 }
 
-template <typename NumericType>
-[[nodiscard]] Triple<NumericType> Scale(const NumericType pF,
-                                        const Triple<NumericType> &pT) {
-  return {pF * pT[0], pF * pT[1], pF * pT[2]};
+template <typename NumericType, std::size_t D>
+[[nodiscard]] std::array<NumericType, D>
+Scale(const NumericType pF, const std::array<NumericType, D> &pT) {
+  std::array<NumericType, D> rr;
+  for (size_t i = 0; i < D; ++i) {
+    rr[i] = pF * pT[i];
+  }
+  return rr;
 }
 
-template <typename NumericType>
-Triple<NumericType> ScaleAdd(const Triple<NumericType> &mult,
-                             const Triple<NumericType> &add,
-                             const NumericType fac) {
-  return {add[0] + mult[0] * fac, add[1] + mult[1] * fac,
-          add[2] + mult[2] * fac};
+template <typename NumericType, std::size_t D>
+std::array<NumericType, D> ScaleAdd(const std::array<NumericType, D> &mult,
+                                    const std::array<NumericType, D> &add,
+                                    const NumericType fac) {
+  std::array<NumericType, D> rr;
+  for (size_t i = 0; i < D; ++i) {
+    rr[i] = add[i] + mult[i] * fac;
+  }
+  return rr;
 }
 
 template <typename NumericType, size_t D>
@@ -135,22 +159,23 @@ ComputeNormal(const Triple<Triple<NumericType>> &planeCoords) {
   return CrossProduct(uu, vv);
 }
 
-template <typename NumericType>
-bool IsNormalized(const Triple<NumericType> &vec) {
+template <typename NumericType, std::size_t D>
+bool IsNormalized(const std::array<NumericType, D> &vec) {
   constexpr double eps = 1e-4;
   auto norm = Norm(vec);
   return std::fabs(norm - 1) < eps;
 }
 
 /* ------------- Debug convenience functions ------------- */
-template <typename NumericType>
-void Print(const Triple<NumericType> &vec, bool endl = true) {
-  std::cout << "(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ")"
-            << (endl ? "\n" : "");
-}
-
-template <typename NumericType> void Print(const Pair<NumericType> &vec) {
-  std::cout << "(" << vec[0] << ", " << vec[1] << ")" << std::endl;
+template <typename NumericType, std::size_t D>
+void Print(const std::array<NumericType, D> &vec, bool endl = true) {
+  std::cout << "(";
+  for (size_t i = 0; i < D; ++i) {
+    std::cout << vec[i];
+    if (i < D - 1)
+      std::cout << ", ";
+  }
+  std::cout << ")" << (endl ? "\n" : "");
 }
 
 template <typename NumericType>
