4da048719434bbffe32e09f9669cff74daef93c7
Curve <fynnbwdt@gmail.com>
2024-05-23 19:21:15 +0200

    refactor(python): simplify function

--- utils/python.cmake
+++ utils/python.cmake
@@ -1,11 +1,7 @@
-function(viennacore_setup_binding NAME OUTPUT_DIR FLAGS DEPENDENCIES)
+function(viennacore_setup_binding NAME OUTPUT_DIR)
   set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${OUTPUT_DIR}/${NAME})
 
   pybind11_add_module(${NAME} "pyWrap.cpp")
-
-  target_link_libraries(${NAME} PUBLIC ${DEPENDENCIES})
-  target_compile_definitions(${NAME} PRIVATE ${FLAGS})
-
   configure_file(__init__.py.in ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/__init__.py)
 
   install(TARGETS ${NAME} LIBRARY DESTINATION ${NAME})
