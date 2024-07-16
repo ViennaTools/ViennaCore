b80199239031ce1f129a30b5a2c3d4268e3a274c
Curve <fynnbwdt@gmail.com>
2024-07-16 16:24:09 +0200

    feat: add setup utilities

---
+++ utils/setup.cmake
@@ -0,0 +1,31 @@
+function(viennacore_setup_copy SOURCE_TARGET DEST_TARGET OUTPUT)
+  if(NOT TARGET ${SOURCE_TARGET})
+    message(WARNING "Could not find target ${SOURCE_TARGET}")
+    return()
+  endif()
+
+  add_custom_command(
+    TARGET ${DEST_TARGET}
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_directory $<TARGET_FILE_DIR:${SOURCE_TARGET}> ${OUTPUT})
+endfunction()
+
+function(viennacore_setup_vtk_env TARGET OUTPUT)
+  message(STATUS "Setting up VTK-Environment for ${TARGET}")
+
+  # We expect all of the VTK binaries to be present in the same directory to which "vtksys" is
+  # built. This is currently the case, and has been the case for prior vtk versions - However we
+  # should keep an eye on this.
+
+  viennacore_setup_copy(vtksys ${TARGET} ${OUTPUT})
+endfunction()
+
+function(viennacore_setup_tbb_env TARGET OUTPUT)
+  message(STATUS "Setting up TBB-Environment for ${TARGET}")
+  viennacore_setup_copy(tbb ${TARGET} ${OUTPUT})
+endfunction()
+
+function(viennacore_setup_embree_env TARGET OUTPUT)
+  message(STATUS "Setting up Embree-Environment for ${TARGET}")
+  viennacore_setup_copy(embree ${TARGET} ${OUTPUT})
+endfunction()
