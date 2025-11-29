83ef5ae90d82ba02464aafad447bdf818c75e0b1
Tobias Reiter <t.reiter1@live.de>
2025-11-29 10:13:27 +0100

    Cast time point explcitly in timer

--- include/viennacore/vcTimer.hpp
+++ include/viennacore/vcTimer.hpp
@@ -8,8 +8,9 @@ template <class Clock = std::chrono::high_resolution_clock> struct Timer {
   using TimePoint = typename Clock::time_point;
 
   TimePoint start_;
-  typename Clock::duration::rep totalDuration = 0.; // in ns
-  typename Clock::duration::rep currentDuration;    // in ns
+  typename Clock::duration::rep totalDuration =
+      static_cast<typename Clock::duration::rep>(0); // in ns
+  typename Clock::duration::rep currentDuration;     // in ns
 
   void start() { start_ = Clock::now(); }
   void finish() {
@@ -19,8 +20,8 @@ template <class Clock = std::chrono::high_resolution_clock> struct Timer {
     totalDuration += currentDuration;
   }
   void reset() {
-    currentDuration = 0.;
-    totalDuration = 0.;
+    currentDuration = static_cast<typename Clock::duration::rep>(0);
+    totalDuration = static_cast<typename Clock::duration::rep>(0);
   }
 };
 
