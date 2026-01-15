dffe18135f9a36bb45bf7e569b432e1f8bc071e0
Tobias Reiter <t.reiter1@live.de>
2026-01-15 09:33:31 +0100

    chore: format

--- tests/openmp/openmp.cpp
+++ tests/openmp/openmp.cpp
@@ -1,11 +1,11 @@
 #include <omp.h>
 
 int main() {
-	int sum = 0;
-	#pragma omp parallel
-	{
-		#pragma omp atomic
-		sum += 1;
-	}
-	return sum;
+  int sum = 0;
+#pragma omp parallel
+  {
+#pragma omp atomic
+    sum += 1;
+  }
+  return sum;
 }
